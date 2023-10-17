e16114806ed41543c97b5760e6b513752d705d2b
RAFAT117 <rafatalbadri@yahoo.com>
2023-10-17 16:05:24 +0200

    write acc info to firestore

--- screens/firebaseConfig.js
+++ screens/firebaseConfig.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyDnuS5uqdOfXsw1WDOtlCIkRmNa4l5ppjI",
   authDomain: "reactnative-chatapp-534e6.firebaseapp.com",
@@ -19,6 +17,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
   
   export default firebaseConfig;
   
