9a5cac2b9254f751355531d856802a9c2b0ec81d
RAFAT117 <rafatalbadri@yahoo.com>
2023-10-24 19:27:04 +0200

    cleaner codes and added comments

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import AppStyles from '../Styling/AppStyles';  // Adjust the path accordingly
+import AppStyles from '../Styling/AppStyles'; 
 
 
 const WelcomeScreen = ({ navigation }) => {
   return (
     <View style={AppStyles.container}>
       <Text style={AppStyles.header}>Welcome</Text>
+
       <TouchableOpacity style={AppStyles.loginButton} onPress={() => navigation.navigate('Login')}>
         <Text style={AppStyles.buttonText}>Login</Text>
       </TouchableOpacity>
+
       <TouchableOpacity style={AppStyles.registerButton} onPress={() => navigation.navigate('Register')}>
         <Text style={AppStyles.buttonText}>Register</Text>
       </TouchableOpacity>
+
     </View>
   );
 }
